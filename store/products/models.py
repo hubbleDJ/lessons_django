8c9d34e40af351a11f4e9a85417040832031897a
Стромецкий Максим Викторович <MVStrometskiy@sberbank.ru>
2023-03-19 18:46:03 +0300

    Добавил товары в БД

--- store/products/models.py
+++ store/products/models.py
@@ -7,10 +7,16 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    def __str__(self) -> str:
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
     quantity = models.PositiveBigIntegerField(default=0)
     image = models.ImageField(upload_to="products_image")
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'''{self.name} | В наличии: {self.quantity}'''
