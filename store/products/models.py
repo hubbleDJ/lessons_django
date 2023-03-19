1a1f188c5d8f8951b43080fdeecf68c0055deb90
hubbleDJ <makscum@gmail.com>
2023-03-19 16:31:24 +0300

    Первый коммит

---
+++ store/products/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+# Модели == таблицы
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveBigIntegerField(default=0)
+    image = models.ImageField(upload_to="products_image")
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
