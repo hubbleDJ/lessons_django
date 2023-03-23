ee687a4645b271c39006c3cb88794f1def333aef
Стромецкий Максим Викторович <MVStrometskiy@sberbank.ru>
2023-03-23 18:57:02 +0300

    Коммит перед закрытием

--- store/users/views.py
+++ store/users/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from users.forms import UserLoginForm
 
 
 def login(request):
     context = {
         'title': 'Store - Авторизация',
+        'form': UserLoginForm()
     }
     return render(request, 'users/login.html', context)
 
