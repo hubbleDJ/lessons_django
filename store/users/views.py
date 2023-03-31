d9ee22c7aa7a0a2e9771b9f780533d2fff083b40
hubbleDJ <makscum@gmail.com>
2023-03-31 17:59:59 +0300

    Добавил авторизацию

--- store/users/views.py
+++ store/users/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from users.forms import UserLoginForm
-
+from django.contrib import auth
+from django.urls import reverse
 
 def login(request):
-    context = {
-        'title': 'Store - Авторизация',
-        'form': UserLoginForm()
-    }
+    if request.method == 'POST':
+        context = {
+            'form': UserLoginForm(data=request.POST)
+        }
+        if context['form'].is_valid():
+            user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        context = {
+            'form': UserLoginForm()
+        }
+
+    context['title'] = 'Store - Авторизация'
+
     return render(request, 'users/login.html', context)
 
 def registration(request):
