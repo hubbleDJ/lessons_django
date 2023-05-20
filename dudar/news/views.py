f9cc240690f55e87eb030d92d4632f8f2cb38ab8
hubbleDJ <makscum@gmail.com>
2023-05-20 23:56:57 +0300

    update

--- dudar/news/views.py
+++ dudar/news/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
+from django.views.generic import DetailView, UpdateView, DeleteView
 from .models import Articles
 from .forms import ArtclesForm
+
+
 
 def news_home(request):
     data = {
@@ -10,6 +13,24 @@ def news_home(request):
     return render(request, 'news/news_home.html', data)
 
 
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+
+class NewsUpdatelView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+    context_object_name = 'article'
+    form_class = ArtclesForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    success_url = '/news'
+    template_name = 'news/news_delete.html'
+
 def create(request):
     data = {'title': 'Форма для добавления статьи'}
 
@@ -18,7 +39,7 @@ def create(request):
         if data['form'].is_valid():
             data['form'].save()
             data['error'] = 'Статья сохранена'
-            return redirect('home')
+            return redirect('news_home')
         else:
             data['error'] = 'Неверно заполнено'
 
